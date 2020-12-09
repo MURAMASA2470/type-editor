636915c604cdf95bd15c71956414d471d299621b
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-12-10 00:48:27 +0900

    🚧 作業途中

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../assets/css/app.css'
 
-const App = (): JSX.Element => {
+const App: React.FC = (): JSX.Element => {
+
+  const [ sentence, setSentence ] = useState('')
+
+  const handleInput = (e: React.SyntheticEvent<EventTarget>): void => {
+    // console.log((e.target as HTMLElement).innerText)
+    setSentence((e.target as HTMLElement).innerText)
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>): void => {
+    if(e.key === 'Enter') {
+      e.preventDefault()
+      setSentence('<br>' + sentence)
+      console.log(document.getSelection()?.anchorOffset)
+    }
+  }
+
   return (
-    <>App</>
+    <>
+    <div
+        contentEditable
+        onInput={handleInput}
+        onKeyDown={handleKeyDown}
+        className="sentence"
+    >
+      {sentence}
+    </div>
+    </>
   )
 }
 
